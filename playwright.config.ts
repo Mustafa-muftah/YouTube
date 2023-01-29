1e2082f0954bb398cd5116984258174a734b1560
unknown <mustafa.mufta7@gmail.com>
2023-01-29 02:22:05 +0200

    fix playwright work flow github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,13 @@
 import { PlaywrightTestConfig } from "@playwright/test";
 
+let headless = !(process.env.PLAYWRIGHT_HEADLESS == "false");
 
 const config: PlaywrightTestConfig = {
   timeout:30000,
   retries:0,
   use: {
     baseURL: "http://localhost:5173",
-    headless: false,
+    headless,
     viewport: { width: 800, height: 667 },
     ignoreHTTPSErrors: true,
     video: "retain-on-failure",
